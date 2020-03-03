a3607ab9a80b67de49ba7830cc1e4431b93728a3
James King <hello@jamesmichaelking.com>
2020-03-03 10:42:04 +0000

    Frontend updates

--- src/SFA.DAS.CareersAdvice/SFA.DAS.CareersAdvice.Web/gulpfile.js
+++ src/SFA.DAS.CareersAdvice/SFA.DAS.CareersAdvice.Web/gulpfile.js
@@ -5,8 +5,21 @@ const sass = require('gulp-sass');
 const paths = require('./config/paths.json');
 const sassOptions = require('./config/sassOptions.js');
 
-gulp.task('app-compile-sass', function() {
-return gulp.src(paths.src.default)
-    .pipe(sass(sassOptions))
-    .pipe(gulp.dest(paths.dist.default));
+gulp.task('app-watch-sass', function() {
+    return gulp.watch(paths.src.default, gulp.series('app-compile-sass')).on('change', function (file) {
+      console.log(`File ${file} was changed, running tasks...`);
+    });
 });
+
+gulp.task('app-compile-sass', function() {
+    return gulp.src(paths.src.default)
+        .pipe(sass(sassOptions))
+        .pipe(gulp.dest(paths.dist.default));
+});
+
+gulp.task('app-copy-assets', function() {
+    return gulp.src(['./node_modules/govuk-frontend/govuk/assets/**/*']).pipe(gulp.dest('./wwwroot/govuk-assets/'));
+});
+
+
+gulp.task('app',  gulp.series('app-compile-sass', 'app-watch-sass'));
